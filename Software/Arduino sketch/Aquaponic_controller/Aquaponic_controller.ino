0dbd7a3317444f5815b5b10f6f85444a44cc45f2
Martin Mortensen <martin_mortensen@rocketmail.com>
2019-06-15 11:06:49 -0700

    reads from serial port and responds to HTML requests now.

--- Software/Arduino sketch/Aquaponic_controller/Aquaponic_controller.ino
+++ Software/Arduino sketch/Aquaponic_controller/Aquaponic_controller.ino
@@ -5,34 +5,65 @@
  */
 
 #include <ESP8266WiFi.h>            
-#include <ESP8266WebServer.h>
+//#include <ESP8266WebServer.h>
+#include <ESPAsyncWebServer.h>
+
 
 #include "index.h" //Our webpage HTML contents
 #include "CSS.h" //Our webpage CSS contents
 #include "js.h" //Our webpage Java script contents
 
 //SSID and Password of your WiFi router
-const char* ssid = "2WIRE975";
-const char* password = "1985984603";
+//const char* ssid = "2WIRE975";
+//const char* password = "1985984603";
+const char* ssid = "MMMwireless";
+const char* password = "lorimart";
 
-ESP8266WebServer server(80); //Server on port 80
+//ESP8266WebServer server(80);  //Server on port 80
+AsyncWebServer server(80);  // Create AsyncWebServer object on port 80
 
+String inputString = "";      // a String to hold incoming data
+bool stringComplete = false;  // whether the string is complete
+
+String temperatureString, levelString, powerString, lightString;
+
+// Replaces placeholders with values
+String processor(const String& var){
+  Serial.print("var: ");
+  Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return (temperatureString);
+    Serial.println(temperatureString);
+  }
+  else if(var == "LEVEL"){
+    return (levelString);
+    Serial.println(levelString);
+  }
+  else if(var == "POWER"){
+    return (powerString);
+    Serial.println(powerString);
+  }
+    else if(var == "LIGHT"){
+    return (lightString);
+    Serial.println(lightString);
+  }
+  return String();
+}
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Begin");
 
-//  Serial.println(indexString);
-//  Serial.println(cssString);
-//  Serial.println(jsString);
-  
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
- 
+//  dht.begin();
+
+  inputString.reserve(200); // reserve 200 bytes for the inputString:
+
+  WiFi.begin(ssid, password);     //Connect to your WiFi router 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-  }
- 
+  } 
   //If connection successful show IP address in serial monitor
   Serial.println("");
   Serial.print("Connected to ");
@@ -40,28 +71,113 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());  //IP address assigned to your ESP
 
-  //server.on("/", handleRoot);      //Which routine to handle at root location
-  
-  server.on(
-    "/", []() {                     //Define the handling function for root path (HTML message)
-    server.send(200, "text/html", indexString);
+  // routines to handle server requests
+//  server.on(
+//    "/", []() {                     //Define the handling function for root path (HTML message)
+//    server.send(200, "text/html", indexString);
+//  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", indexString, processor);
   });
   
-  server.on("/script.js", []() { //Define the handling function for the javascript path
-    server.send(200, "text/javascript", jsString);
+//  server.on("/script.js", []() { //Define the handling function for the javascript path
+//    server.send(200, "text/javascript", jsString);
+//  });
+  server.on("/script.js", HTTP_GET, [] (AsyncWebServerRequest *request){ //Define the handling function for the javascript path
+    request->send_P(200, "text/javascript", jsString, processor);
   });
   
-  server.on("/style.css", []() { //Define the handling function for the CSS path
-    server.send(200, "text/css", cssString);
+//  server.on("/style.css", []() { //Define the handling function for the CSS path
+//    server.send(200, "text/css", cssString);
+//  });
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) { //Define the handling function for the CSS path
+    request->send_P(200, "text/css", cssString, processor);
+  });
+
+//  server.on("/temp", []() { //Define the handling function for the temp value
+//    server.send(200, "text/plain", temperatureString);
+//  });
+  server.on("/temp", HTTP_GET, [](AsyncWebServerRequest *request) { //Define the handling function for the temp value
+//    request->send_P(200, "text/plain", temperatureString.c_str, processor);
+    int strLen = temperatureString.length() + 1;
+    char tempArray[strLen];
+    temperatureString.toCharArray(tempArray, strLen);
+    request->send_P(200, "text/plain", tempArray, processor);
+  });
+
+//  server.on("/level", []() { //Define the handling function for the temp value
+//    server.send(200, "text/plain", levelString);
+//  });
+  server.on("/level", HTTP_GET, [](AsyncWebServerRequest *request) { //Define the handling function for the temp value
+//    request->send_P(200, "text/plain", levelString.c_str, processor);
+    int strLen = levelString.length() + 1;
+    char levelArray[strLen];
+    levelString.toCharArray(levelArray, strLen);
+    request->send_P(200, "text/plain", levelArray, processor);
+  });
+
+//  server.on("/power", []() { //Define the handling function for the temp value
+//    server.send(200, "text/plain", powerString);
+//  });
+  server.on("/power", HTTP_GET, [](AsyncWebServerRequest *request) { //Define the handling function for the temp value
+//    request->send_P(200, "text/plain", powerString.c_str, processor);
+    int strLen = powerString.length() + 1;
+    char powerArray[strLen];
+    powerString.toCharArray(powerArray, strLen);
+    request->send_P(200, "text/plain", powerArray, processor);
+  });
+
+//  server.on("/light", []() { //Define the handling function for the temp value
+//    server.send(200, "text/plain", lightString);
+//  });
+  server.on("/light", HTTP_GET, [](AsyncWebServerRequest *request) { //Define the handling function for the temp value
+//    request->send_P(200, "text/plain", lightString.c_str, processor);
+    int strLen = lightString.length() + 1;
+    char lightArray[strLen];
+    lightString.toCharArray(lightArray, strLen);
+    request->send_P(200, "text/plain", lightArray, processor);
   });
 
   server.begin();                  //Start server
   Serial.println("HTTP server started");
-
 }
 
 void loop() {
-  server.handleClient();          //Handle client requests
-
-
+  String junk;
+  
+//  server.handleClient();          //Handle client requests
+  
+  // read data from the serial port (from Arduino) 
+  // and parse out temp, level, power, and light data.
+  if (Serial.available()) {
+    temperatureString = Serial.readStringUntil(':');
+    levelString = Serial.readStringUntil(':');
+    powerString = Serial.readStringUntil(':');
+    lightString = Serial.readStringUntil(':');
+    junk = Serial.readStringUntil('\n');    
+    
+    if (temperatureString.length() > 2){
+ //     Serial.print(temperatureString.length());
+      Serial.print("temp:");
+      Serial.println(temperatureString);
+    }
+    if (levelString.length() > 2 ){
+//      Serial.print(levelString.length());
+      Serial.print("Level:");
+      Serial.println(levelString);
+    }
+    if (powerString.length() > 2){
+//      Serial.print(powerString.length());
+      Serial.print("power:");
+      Serial.println(powerString);
+    }
+    if (lightString.length() > 2){
+//      Serial.print(lightString.length());
+      Serial.print("light:");
+      Serial.println(lightString);
+    } 
+    // clear the string:
+    inputString = "";
+    stringComplete = false;
+  }
 }
--- Software/Web page/source page/script.js
+++ Software/Web page/source page/script.js
@@ -1,73 +1,124 @@
 // Our labels along the x-axis
-//var years = [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050];
-var time = [12,1,2,3,4,5,6,7,8,9];
+var time = "1,2,3,4,5,6,7,8,9,10,11,12";
 // For drawing the lines
-var temp = [20,30,40,50,60,70,80,75,80,70];
-var level = [50,60,50,40,50,60,50,40,50,60];
-var power = [5,6,5,4,5,6,5,4,5,6];
-var light = [0.5,0.5,0.5,1.5,1.5,1.5,0.5,0.5,1.5,1.5];
-var northAmerica = [6,3,2,2,7,26,82,172,312,433];
+var temp = "20,30,40,50,60,70,80,75,80,70";
+var level = "50,60,50,40,50,60,50,40,50,60";
+var power = "5,6,5,4,5,6,5,4,5,6";
+var light = "0.5,0.5,0.5,1.5,1.5,1.5,0.5,0.5,1.5,1.5";
 
+// temperature chart
 var ctx = document.getElementById("tempChart");
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: time,
+    labels: time.split(','),
     datasets: [
       { 
-        data: temp,
+        data: temp.split(','),
+//        data: %TEMPERATURE%.split(','),
         label: "Temperature",
-        borderColor: "#3e95cd",
+        borderColor: "#FFC12B",
 		fill: false
       }
     ]
   }
 });
 
+// water level chart
 var ctx = document.getElementById("levelChart");
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: time,
+    labels: time.split(','),
     datasets: [
 	  { 
-   		data: level,
+   		data: level.split(','),
+//      data: %LEVEL%.split(','),
    		label: "Water Level",
-   		borderColor: "#8e5ea2",
+   		borderColor: "#9F3BFF",
    		fill: false
  	  }
     ]
   }
 });
 
+// Power chart
 var ctx = document.getElementById("powerChart");
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: time,
+    labels: time.split(','),
     datasets: [
 	  { 
-   		data: power,
+   		data: power.split(','),
+//      data: %POWER%.split(','),
    		label: "Power Consumption",
-   		borderColor: "#8e5ea2",
+   		borderColor: "#37B0FF",
    		fill: false
  	  }
     ]
   }
 });
 
+// light power chart
 var ctx = document.getElementById("lightChart");
 var myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: time,
+    labels: time.split(','),
     datasets: [
 	  { 
-   		data: light,
+      data: light.split(','),
+ //    data: %LIGHT%.split(','),
    		label: "Lighting power Consumption",
-   		borderColor: "#8e5ea2",
+   		borderColor: "#FF2B30",
    		fill: false
  	  }
     ]
   }
 });
+
+// update interval
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temp").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temp", true);
+  xhttp.send();
+}, 60000 ) ; // repeat once a minute
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("level").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/level", true);
+  xhttp.send();
+}, 60000 ) ; // repeat once a minute
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("power").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/power", true);
+  xhttp.send();
+}, 60000 ) ; // repeat once a minute
+
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("light").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/light", true);
+  xhttp.send();
+}, 60000 ) ; // repeat once a minute
